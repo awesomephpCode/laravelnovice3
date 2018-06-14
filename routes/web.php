d20a3971ad80e42bfa35cd4e9499ce0cb5d42bb6
1785301169@qq.com <1785301169@qq.com>
2018-06-14 15:36:34 +0800

    完成用户注册

--- routes/web.php
+++ routes/web.php
@@ -6,8 +6,4 @@ Route::get('/help', 'StaticPagesController@help')->name('help');
 Route::get('/about', 'StaticPagesController@about')->name('about');
 
 Route::get('signup', 'UsersController@create')->name('signup');
-
-
-Route::get('test',function (){
-    echo route('home');
-});
+Route::resource('users', 'UsersController');
