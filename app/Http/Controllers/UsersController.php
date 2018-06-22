78d4c83a50f942e1c2e14beaf425ff18a082675e
1785301169@qq.com <1785301169@qq.com>
2018-06-22 17:55:24 +0800

    添加用户关注

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -139,4 +139,19 @@ class UsersController extends Controller
         return back();
     }
 
+    //用户关注列表
+    public function followings(User $user)
+    {
+        $users = $user->followings()->paginate(30);
+        $title = '关注的人';
+        return view('users.show_follow', compact('users', 'title'));
+    }
+
+    //用户粉丝列表
+    public function followers(User $user)
+    {
+        $users = $user->followers()->paginate(30);
+        $title = '粉丝';
+        return view('users.show_follow', compact('users', 'title'));
+    }
 }
